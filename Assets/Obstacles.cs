673447a00ac3d8d6b12734d657c34785cf7ce29e
AtulSingh2000 <atulkumar102s@gmail.com>
2020-08-02 16:19:02 +0530

    Initial commit

---
+++ Assets/Obstacles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    float speed;
+
+    void Start()
+    {
+        speed = 2.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        godown();
+
+    }
+
+    void godown()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/TriggerGameOver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TriggerGameOver : MonoBehaviour
+{
+    private UIManager UI;
+    public GameObject GameOverPanel;
+    public GameObject GameWinPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameOverPanel.SetActive(false);
+        Vector3 tmpPos = transform.position;
+        tmpPos.x = Mathf.Clamp(tmpPos.x, -1.8f, 1.8f);
+        transform.position = tmpPos;
+
+        UI = GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Wall"))
+        {
+           // Debug.Log("Yes!!");
+            GameOverPanel.SetActive(true);
+            //Time.timeScale = 0;
+           
+        }
+        if(other.gameObject.CompareTag("Call"))
+        {
+            GameWinPanel.SetActive(true);
+            Debug.Log("Win");
+        }
+    }
+
+    public void Restart()
+    {
+
+        UI.Restar();
+    }
+}
+
